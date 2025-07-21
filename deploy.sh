dc45af76efc05ca6d6df12aba87bb0447d600f70
Sohel Sura <sohelsura@gmail.com>
2025-07-21 19:29:44 +0000

    Enhance deployment script and update image paths for consistency across templates

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ echo "Building and starting the Django application with Docker..."
 
 sudo docker compose -f docker-compose.yml up --build -d
 
+sudo docker compose exec web python manage.py collectstatic --noinput
+
 echo "Waiting for database to be ready..."
 sleep 10
 
